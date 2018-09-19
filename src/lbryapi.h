889d76b4c93ee4844f15000def206008537a3ca7
Oleg Silkin <o.silkin98@gmail.com>
2018-09-18 19:10:40 -0500

    Adds include directives for project namespace structure and string

--- src/lbryapi.h
+++ src/lbryapi.h
@@ -1,3 +1,8 @@
+// include/string is used for passing strings in and out of functions
+#include <string>   // includes the definition for std::string
+#include <cstddef>  // Includes the definition for size_t
+#include "namespace.h"
+
 #ifndef CPPBRY_LBRYAPI_H
 #define CPPBRY_LBRYAPI_H
 
