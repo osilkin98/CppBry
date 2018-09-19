0b2031473c3daad754142e260ae06887f98e520a
Oleg Silkin <o.silkin98@gmail.com>
2018-09-18 23:21:45 -0500

    Defines request_id and make_request functions

--- src/lbryapi.cpp
+++ src/lbryapi.cpp
@@ -2,4 +2,16 @@
 // Created by oleg on 9/18/18.
 //
 
+#include <string>   // includes the definition for string
+#include <cstddef>  // Includes the definition for size_t
+#include <list>
+#include <utility>
 #include "lbryapi.h"
+
+size_t lbry::BaseApi::request_id = 0;
+
+json lbry::BaseApi::make_request(const std::string& url, const std::string& method,
+                                      std::map<std::string, std::string> params, const std::string& username,
+                                      const std::string& password) {
+
+}
