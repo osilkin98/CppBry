4407acd694d75c16385a7a3f0ffa6e1d15404f4d
Oleg Silkin <o.silkin98@gmail.com>
2018-09-18 19:51:01 -0500

    Declares the BaseApi body

--- src/lbryapi.h
+++ src/lbryapi.h
@@ -1,10 +1,29 @@
 // include/string is used for passing strings in and out of functions
 #include <string>   // includes the definition for std::string
 #include <cstddef>  // Includes the definition for size_t
+#include <utility>
 #include "namespace.h"
 
 #ifndef CPPBRY_LBRYAPI_H
 #define CPPBRY_LBRYAPI_H
+
+class lbry::BaseApi {
+    static size_t request_id;
+
+    /** Makes a POST Request to the given `URL` with the specified method with the given params and the authorization
+     *
+     * @param url URL to which we make the POST request to
+     * @param method Method we wish to call from the API
+     * @param params Parameters
+     * @param basic_auth
+     * @return
+     */
+
+    template <typename Container>
+    static std::string make_request(std::string url, std::string method, Container<std::string> params,
+            std::pair<std::string> basic_auth);
+
+};
 
 
 
