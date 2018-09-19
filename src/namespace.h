dc8b1a98644c57378576a8d1e73e5ce259ad20a3
Oleg Silkin <o.silkin98@gmail.com>
2018-09-18 19:07:22 -0500

    Creates namespace structure

---
+++ src/namespace.h
@@ -0,0 +1,20 @@
+//
+// Created by oleg on 9/18/18.
+//
+
+#ifndef LBRY_NAMESPACE_H
+#define LBRY_NAMESPACE_H
+
+namespace lbry {
+
+    // declare that the base class exists
+    class BaseApi;
+
+    /* These two inherit from the BaseApi */
+    class LbryApi;
+    class LbryCrdApi;
+
+};
+
+
+#endif //CPPBRY_NAMESPACE_H
