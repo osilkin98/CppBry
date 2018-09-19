81dd4fc3d2803067f5061b4c38d56a467930a889
Oleg Silkin <o.silkin98@gmail.com>
2018-09-18 23:42:55 -0500

    Fixes imports

--- src/lbryapi.cpp
+++ src/lbryapi.cpp
@@ -2,16 +2,17 @@
 // Created by oleg on 9/18/18.
 //
 
-#include <string>   // includes the definition for string
-#include <cstddef>  // Includes the definition for size_t
-#include <list>
-#include <utility>
+#include <string>    // includes the definition for string
+#include <cstddef>   // Includes the definition for size_t
+#include <map>
 #include "lbryapi.h"
+#include "../include/json.hpp"
 
 size_t lbry::BaseApi::request_id = 0;
 
 json lbry::BaseApi::make_request(const std::string& url, const std::string& method,
                                       std::map<std::string, std::string> params, const std::string& username,
                                       const std::string& password) {
+
 
 }
