3cd7cfc476974fd29688d258818bb7cf0a51b4af
Oleg Silkin <o.silkin98@gmail.com>
2018-09-21 01:16:38 -0500

    Moves curlpp and json to a third_parties directory

--- install.sh
+++ install.sh
@@ -8,14 +8,14 @@
 #
 #
 # To run the CMakeLists file in order to build the necessary targets
-cmake curlpp/CMakeLists.txt
+cmake third_party/curlpp/CMakeLists.txt
 
 # This runs the command in a sub-shell so that way
 # The parent script stays in the same directory
 # And then it builds all the object files we need
 # And then it installs all the file by writing them to their
 # Necessary locations within the install target in the makefile
-(cd curlpp/ && make && make install)
+(cd third_party/curlpp/ && make && make install)
 
 
 ######################################################################
@@ -25,9 +25,9 @@ cmake curlpp/CMakeLists.txt
 
 # Again to build the necessary targets in order for the makefile
 # To correctly execute
-cmake json/CMakeLists.txt
+cmake third_party/json/CMakeLists.txt
 
 
 # Then do the same thing as before, simply cd into it and 
 # Then run make and make install
-(cd json/ && make && make install)
+(cd third_party/json/ && make && make install)
